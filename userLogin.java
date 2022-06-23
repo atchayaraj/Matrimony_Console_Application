7561e494739c459f75bf0bb0cf50cf8a76bce26c
atchayaraj <72649598+atchayaraj@users.noreply.github.com>
2022-06-23 15:48:31 +0530

    Create userLogin.java

---
+++ userLogin.java
@@ -0,0 +1,51 @@
+import java.util.*;
+
+enum userChoice { SEARCHFILTER, USERPRIVACY}
+
+class userLogin{
+	String userName;
+	String passWord;
+	boolean flag;
+	int choiceNumber=1;
+	void checkCredentials() {
+		System.out.println("Enter the username : ");
+		System.out.println("Enter the Password : ");
+		databaseInt di = new dataBase();
+		flag = di.checkUserLoginCredentials(userName,passWord);
+		if(flag==true) {
+			System.out.println("You successfully loged-in ");
+			flag=di.checkUserIsExisting(userName);
+			if(flag==true)
+			{
+				userChoice();
+			}
+			else
+			{
+				threeStepInformation  si = new threeStepInformation(userName);
+				di.addUserThreeStepsInformation(si);
+				userChoice();
+			}
+		}
+	}
+	void userChoice() {
+		new displayProfiles();
+		userChoice uc = userChoice.values()[choiceNumber];
+		switch(uc)
+		{
+		case SEARCHFILTER:
+			searchFilters sf = new searchFilters();
+			sf.filter();
+			break;
+		case USERPRIVACY:
+			userPrivacy up = new userPrivacy();
+			up.setRestriction();
+			databaseInt db = new dataBase();
+			db.addUserPrivacy(userName);
+			break;
+		default:
+			break;
+		}
+		
+	}
+	
+}
