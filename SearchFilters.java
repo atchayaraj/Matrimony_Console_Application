e3e5b7d13874b3616fb2e00fde7d10bc9956ad53
atchayaraj <72649598+atchayaraj@users.noreply.github.com>
2022-06-23 15:50:58 +0530

    Create SearchFilters.java

---
+++ SearchFilters.java
@@ -0,0 +1,11 @@
+class searchFilters{
+		int age = 0;
+		String Religion="";
+		String caste="";
+		String educationalqualification="";
+		String maritalstatus="";
+		String residentialstate="";
+		void filter() {
+			new displayProfiles(age,Religion,caste,educationalqualification,maritalstatus,residentialstate);
+		}
+	}
